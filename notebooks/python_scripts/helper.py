4ad1003ea162aa4fc8b4c75493c8328edb68a1b7
WailAbou <abou.w@hotmail.com>
2021-04-19 14:59:34 +0200

    - Implemented alternative way of downloading (single request)

--- notebooks/python_scripts/helper.py
+++ notebooks/python_scripts/helper.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sentinelhub import SentinelHubRequest, BBox, CRS, DataCollection
 from sentinelhub.constants import MimeType
 
+
 def plot_image(image, factor=1/255, clip_range=(0, 1), **kwargs):
     """
     Utility function for plotting RGB images.
@@ -16,17 +17,15 @@ def plot_image(image, factor=1/255, clip_range=(0, 1), **kwargs):
     ax.set_yticks([])
 
 
-def send_request(evalscript, input_data,  coords, config):
+def send_request(evalscript, input_data, coords, config, save_data):
     bbox = BBox(bbox=coords, crs=CRS.WGS84)
-    request = SentinelHubRequest(evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512, 512], 
-    responses=[ SentinelHubRequest.output_response('default', MimeType.PNG)])
-    response = request.get_data()[0]
+    request = SentinelHubRequest(data_folder='../data/images', evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512, 512], responses=[SentinelHubRequest.output_response('default', MimeType.PNG)])
+    response = request.get_data(save_data=save_data)[0]
     return response
 
 
-simple_request =  lambda eval_script, coords, config, **kwargs: send_request(
+simple_request =  lambda eval_script, coords, config, save_data, **kwargs: send_request(
     eval_script, 
-    [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=('2020-02-05', '2020-04-25'),
-                            other_args ={"dataFilter": {"maxCloudCoverage":0}})], 
-    coords, config
+    [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=('2020-02-05', '2020-04-25'), **kwargs)], 
+    coords, config, save_data
 )
