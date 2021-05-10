932e0391aa2ad14d0e2ddb83e9bcdfbe66b3ace8
AmaryllisLee <ama.lee@student.hu.nl>
2021-05-10 16:09:55 +0200

    add sentinel_request function

--- notebooks/python_scripts/helper.py
+++ notebooks/python_scripts/helper.py
@@ -19,13 +19,21 @@ def plot_image(image, factor=1/255, clip_range=(0, 1), **kwargs):
 
 def send_request(evalscript, input_data, coords, config, save_data):
     bbox = BBox(bbox=coords, crs=CRS.WGS84)
-    request = SentinelHubRequest(data_folder='../data/images', evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512, 512], responses=[SentinelHubRequest.output_response('default', MimeType.PNG)])
+    request = SentinelHubRequest(data_folder='../data/images', evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512, 512], responses=[SentinelHubRequest.output_response('default', MimeType.TIFF)])
     response = request.get_data(save_data=save_data)[0]
     return response
 
 
-simple_request =  lambda eval_script, coords, config, save_data, **kwargs: send_request(
+simple_request = lambda eval_script, coords, config, save_data, **kwargs: send_request(
     eval_script, 
     [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=('2020-02-05', '2020-04-25'), **kwargs)], 
     coords, config, save_data
 )
+
+# Function that sends a request for an satellite image of a scene and timestamp 
+sentinel_request = lambda eval_script, coords, time_interval, config, save_data, **kwargs: send_request(
+    eval_script, 
+    [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=time_interval, **kwargs)],
+    coords, config, save_data
+)
+
