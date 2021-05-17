44f9daa18314f5f87264aaa2232da58b81409bf6
AmaryllisLee <ama.lee@student.hu.nl>
2021-05-17 13:11:55 +0200

    add function request geoJSON to get images in json format

--- notebooks/python_scripts/helper.py
+++ notebooks/python_scripts/helper.py
@@ -17,12 +17,17 @@ def plot_image(image, factor=1/255, clip_range=(0, 1), **kwargs):
     ax.set_yticks([])
 
 
-def send_request(evalscript, input_data, coords, config, save_data):
+def send_request(evalscript, input_data, coords, config, save_data, data_folder='../data/images'):
     bbox = BBox(bbox=coords, crs=CRS.WGS84)
-    request = SentinelHubRequest(data_folder='../data/images', evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512, 512], responses=[SentinelHubRequest.output_response('default', MimeType.TIFF)])
+    request = SentinelHubRequest(data_folder=data_folder, evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512,512], responses=[SentinelHubRequest.output_response('default', MimeType.TIFF)])
     response = request.get_data(save_data=save_data)[0]
     return response
 
+def send_request_geojson(evalscript, input_data, coords, config, save_data, data_folder='../data/images'):
+    bbox = BBox(bbox=coords, crs=CRS.WGS84)
+    request = SentinelHubRequest(data_folder=data_folder, evalscript=evalscript, input_data=input_data, bbox=bbox, config=config, size=[512,512], responses=[SentinelHubRequest.output_response('default', MimeType.JSON)])
+    response = request.get_data(save_data=save_data)[0]
+    return response
 
 simple_request = lambda eval_script, coords, config, save_data, **kwargs: send_request(
     eval_script, 
@@ -31,9 +36,15 @@ simple_request = lambda eval_script, coords, config, save_data, **kwargs: send_r
 )
 
 # Function that sends a request for an satellite image of a scene and timestamp 
-sentinel_request = lambda eval_script, coords, time_interval, config, save_data, **kwargs: send_request(
+sentinel_request = lambda eval_script, coords, time_interval, config, save_data, data_folder, **kwargs: send_request(
     eval_script, 
     [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=time_interval, **kwargs)],
-    coords, config, save_data
+    coords, config, save_data, data_folder
+)
+# Function that sends a request for an satellite image of a scene and timestamp and return the image in GeoJSON
+request_geojson = lambda eval_script, coords, time_interval, config, save_data, data_folder, **kwargs: send_request_geojson(
+    eval_script, 
+    [SentinelHubRequest.input_data(data_collection=DataCollection.SENTINEL2_L2A, time_interval=time_interval, **kwargs)],
+    coords, config, save_data, data_folder
 )
 
