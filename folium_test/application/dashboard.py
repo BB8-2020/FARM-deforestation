b36885822e87622553691625e813d40ae901474d
YSLucas <54868396+YSLucas@users.noreply.github.com>
2021-06-03 20:53:04 +0200

    geoshape added to map + popup

--- folium_test/application/dashboard.py
+++ folium_test/application/dashboard.py
@@ -2,15 +2,59 @@ import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import os
+from dash_html_components.Content import Content
 
-app = dash.Dash()
+from dash_html_components.Tr import Tr
 
-FARM_LOGO = ""
-CAPGEMINI_LOGO = ""
+FARM_LOGO = "folium_test/application/images/farm_logo.png"
+CAPGEMINI_LOGO = "folium_test/application/images/capgemini_logo.png"
+
+# print(os.getcwd())
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# navbar = dbc.Navbar(
+#     [
+#         html.A(
+#             # Use row and col to control vertical alignment of logo / brand
+#             dbc.Row(
+#                 [
+#                     html.Img(src="https://d2kta6kx4236p6.cloudfront.net/img/logo.56e22df1.png", height="30px"),
+#                     dbc.Nav(
+#                         [
+#                             dbc.NavItem(dbc.NavLink("Home", active=True, href="#")),
+#                             dbc.NavItem(dbc.NavLink("Interactive Map", href="#"))
+#                         ],
+#                         pills=True
+#                     )
+#                 ],
+#                 align="center",
+#                 no_gutters=True,
+#             ),
+#             href="https://plot.ly",
+#         )
+#     ],
+#     color="white"
+# )
+
+nav_bar = html.Div(children=[
+    html.Nav(className="nav nav-pills", children=[
+        html.Img(src="https://d2kta6kx4236p6.cloudfront.net/img/logo.56e22df1.png", height="30px"),
+        html.A('App1', className="nav-item nav-link btn", href='/apps/App1'),
+        html.A('App2', className="nav-item nav-link btn", href='/apps/App2') 
+])])
+
+bot_bar = html.Div(children=[
+    html.Img(src="https://d2kta6kx4236p6.cloudfront.net/img/capgemini-logo.dd5491a8.png", height="30px")
+    ])
+
+# content = html.Iframe(id='map', srcDoc=open('folium_test//application//index.html', 'r').read(), width='100%', height='2000', style=CONTENT_STYLE)
+
+content = html.Div(id="page-content", children=[html.Iframe(id='map', srcDoc=open('folium_test//application//index.html', 'r').read(), width='100%', height='805')])
 
 app.layout = html.Div([
-    html.H1('Title'),
-    html.Iframe(id = 'map', srcDoc= open('folium_test//application//index.html', 'r').read(), width='100%', height='600')
+    nav_bar,
+    content,
+    bot_bar
 ]
 )
 
--- folium_test/application/map.py
+++ folium_test/application/map.py
@@ -9,8 +9,7 @@ from folium.plugins import Fullscreen
 from folium.plugins import MarkerCluster
 import os
 import pandas as pd
-
-
+from area import area
 
 
 
@@ -24,8 +23,8 @@ min_lat, max_lat = 19, 29
 m = folium.Map(
     # Starting point + Window size
     location=[21.5693, 87.1240],
-    width=1000,
-    height=600,
+    width=1500,
+    height=800,
 
     # Tile settings
     tiles=None,
@@ -66,20 +65,31 @@ for index, row in csv_data.iterrows():
 ######################## GeoJSON #################################
 
 # GeoJSON data
-overlay1 = os.path.join('folium_test//data', 'mask.json')
-overlay2 = os.path.join('folium_test//data', 'result.json')
+overlay1 = os.path.join('folium_test//data//result_geojson', 'result.json')
+# overlay2 = os.path.join('folium_test//data', 'result.json')
 
 
 # add .png to map
-overlay3 = os.path.join('folium_test//data', 'respone_t.png')
-m.add_child(folium.raster_layers.ImageOverlay(overlay3, name='png', opacity=0.8, bounds=[[21.443988, 87.018996], [21.643988, 87.218996]]))
+# overlay3 = os.path.join('folium_test//data', 'respone_t.png')
+# m.add_child(folium.raster_layers.ImageOverlay(overlay3, name='png', opacity=0.8, bounds=[[21.443988, 87.018996], [21.643988, 87.218996]]))
 
 # Create Polygon overlay with GeoJSON data
 style1 = {'fillColor': '#228B22', 'color': '#228B22'}
 style2 = {'fillColor': '#c21f1f', 'color': '#c21f1f'}
 
-folium.GeoJson(overlay2, name='Woods cut (1990)', style_function=lambda x:style2).add_to(m)
-folium.GeoJson(overlay1, name='Woods now', style_function=lambda x:style1).add_to(m)
+# calculate area of geojson shape
+f = open('folium_test/data/result_geojson/result.json',)
+geojson_data = json.load(f)
+total = 0
+for shape in geojson_data['features']:
+    total += area(shape['geometry'])
+
+# folium.GeoJson(overlay2, name='Woods cut (1990)', style_function=lambda x:style2).add_to(m)
+# add geojson shape with popup with area data to map
+geo_txt = f'{str(round(total, 2))} km/2'
+geo_popup = folium.Popup(geo_txt, min_width=100, max_width=200)
+folium.GeoJson(overlay1, name='Woods now', style_function=lambda x:style1).add_child(geo_popup).add_to(m)
+
 
 
 
