ebf5bf4e946d57e4fb0c59b0df35adab14d9b73a
WailAbou <abou.w@hotmail.com>
2021-06-25 02:05:39 +0200

    - Corrected tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,7 +1,11 @@
 """Test functions to check if the helper functions run correctly."""
 from pytest import approx
+from tensorflow import convert_to_tensor
 
 from python.models.metrics import MeanIoU, Precision, Recall
+
+y_true = convert_to_tensor([0, 1, 1, 1])
+y_pred = convert_to_tensor([1, 0, 1, 1])
 
 
 def test_precision():
@@ -9,11 +13,11 @@ def test_precision():
     # Adapted from docs: https://www.tensorflow.org/api_docs/python/tf/keras/metrics/Precision
     # Note: Our custom class expects a probability matrice instead of
     m = Precision()
-    m.update_state([0, 1, 1, 1], [1, 0, 1, 1])
+    m.update_state(y_true, y_pred)
     assert approx(m.result().numpy()) == 0.6666667
 
     m = Precision()
-    m.update_state([0, 1, 1, 1], [1, 0, 1, 1], sample_weight=[0, 0, 1, 0])
+    m.update_state(y_true, y_pred, sample_weight=[0, 0, 1, 0])
     assert m.result().numpy() == 1.0
 
 
@@ -21,11 +25,11 @@ def test_recall():
     """Test whether the adapted Recall class still works as it's baseline counterpart."""
     # Adapted from docs: https://www.tensorflow.org/api_docs/python/tf/keras/metrics/Recall
     m = Recall()
-    m.update_state([0, 1, 1, 1], [1, 0, 1, 1])
+    m.update_state(y_true, y_pred)
     assert approx(m.result().numpy()) == 0.6666667
 
     m = Recall()
-    m.update_state([0, 1, 1, 1], [1, 0, 1, 1], sample_weight=[0, 0, 1, 0])
+    m.update_state(y_true, y_pred, sample_weight=[0, 0, 1, 0])
     assert m.result().numpy() == 1.0
 
 
@@ -33,5 +37,5 @@ def test_meaniou():
     """Test whether the adapted Recall class still works as it's baseline counterpart."""
     # Adapted from docs: https://www.tensorflow.org/api_docs/python/tf/keras/metrics/MeanIoU
     m = MeanIoU(num_classes=2)
-    m.update_state([0, 0, 1, 1], [0, 1, 0, 1])
-    assert approx(m.result().numpy()) == 0.33333334
+    m.update_state(y_true, y_pred)
+    assert approx(m.result().numpy()) == 0.25
